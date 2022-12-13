052d9938babeca74cfb3cdb6a86033a7909b7cb7
Damian Jeleń <damian.jelen.1986@gmail.com>
2022-12-13 13:29:01 +0100

    klasy abstrakcyjne zad 1 change main class

--- src/main/java/klasy_abstrakcyjne/zad_1/Main.java
+++ src/main/java/klasy_abstrakcyjne/zad_1/Main.java
@@ -3,13 +3,13 @@ package klasy_abstrakcyjne.zad_1;
 public class Main {
     public static void main(String[] args) {
         Car car = new Car(150, "Lanos", "Compact");
-        System.out.println(car.getMaxSpeed());
-        System.out.println(car.getType());
-        System.out.println(car.getModel());
-        System.out.println(car.getSound());
-        System.out.println(car.toString());
-
         Train train = new Train(200, "TGV", 500);
-        System.out.println(train.toString());
+
+        Vehicle[] vehicle = {car, train};
+
+        for (Vehicle veh : vehicle) {
+            System.out.println(veh.toString());
+            System.out.println("=============================");
+        }
     }
 }
