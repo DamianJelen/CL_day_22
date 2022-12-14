1d2cd44e4c365e6b9b06492a494a13bf76e531e7
Damian Jeleń <damian.jelen.1986@gmail.com>
2022-12-14 16:47:03 +0100

    interface extend ex 1

--- src/main/java/interfejsy/zad_1/UrlMain.java
+++ src/main/java/interfejsy/zad_1/UrlMain.java
@@ -4,5 +4,8 @@ public class UrlMain {
     public static void main(String[] args) {
         Url standardUrl = new StandardUrl();
         System.out.println(standardUrl.getParam("param1", "url_example?param1=99&param2=string"));
+
+        Url extenderUrl = new ExtenderUrl();
+        System.out.println(extenderUrl.getParam("param2", "url_example/param1.55/param2.byte"));
     }
 }
