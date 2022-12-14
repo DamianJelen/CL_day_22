6a3e96e285155a134da18d3afd00e90522d34f56
Damian Jeleń <damian.jelen.1986@gmail.com>
2022-12-14 16:39:38 +0100

    interface ex 1

---
+++ src/main/java/interfejsy/zad_1/Url.java
@@ -0,0 +1,5 @@
+package interfejsy.zad_1;
+
+public interface Url {
+    String getParam(String name, String url);
+}
---
+++ src/main/java/interfejsy/zad_1/UrlMain.java
@@ -0,0 +1,8 @@
+package interfejsy.zad_1;
+
+public class UrlMain {
+    public static void main(String[] args) {
+        Url standardUrl = new StandardUrl();
+        System.out.println(standardUrl.getParam("param1", "url_example?param1=99&param2=string"));
+    }
+}
